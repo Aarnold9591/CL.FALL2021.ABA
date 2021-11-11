9052d48779f4a48c1890d3b961245023a6a77a44
Aarnold9591 <89933476+Aarnold9591@users.noreply.github.com>
2021-11-11 15:43:07 -0500

    Email Validation

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,19 @@
-console.log('Testing');
+
+//----Email Validation----
+function validateEmail(emailId)
+{
+var mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+if(emailId.value.match(mailformat))
+{
+alert("Thank you for subscribing!")    
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("Invalid email address.");
+document.form1.text1.focus();
+return false;
+}
+
+}
